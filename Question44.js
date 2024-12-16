bd667a95df6f093bbb9f42fddfce0c9612ff2053
Ayush <ayush0340.be21@chitkara.edu.in>
2024-12-16 15:32:54 +0530

    [Ayush] Mod: Update README file

--- Question44.js
+++
@@ -1,9 +0,0 @@
-// 44. Create a function that will convert a string containing a binary number into a number
-
-let str = "10101"
-
-function binaryToDecimal(str) {
-    return parseInt(str, 2);
-}
-
-console.log(binaryToDecimal(str)); 
